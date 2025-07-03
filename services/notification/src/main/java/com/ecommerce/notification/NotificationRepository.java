a8a32a3952c349dc6d4124c5220a22a15240ca40
fayob <adedejifavour6@gmail.com>
2025-07-03 09:34:28 +0100

    Add notification service

---
+++ services/notification/src/main/java/com/ecommerce/notification/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerce.notification;
+
+import com.ecommerce.kafka.payment.PaymentConfirmation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+}
